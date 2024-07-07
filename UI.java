556719a792c935d28a4e1e86cdd294d0a48b78dd
Smaghead <kelemen.da@gmail.com>
2024-07-07 22:42:14 +0200

    Create UI.java

---
+++ UI.java
@@ -0,0 +1,26 @@
+public class UI {
+    private Scanner scanner;
+    private PointCalculator pointCalc;
+    
+    public UI(Scanner scanner, PointCalculator pointCalc) {
+        this.scanner = new Scanner(System.in);
+        this.pointCalc = new PointCalculator();
+    }
+    
+    public void start() {
+        System.out.println("Enter point totals, -1 stops:");
+        while (true) {
+            int input = scanner.nextInt();
+            if (input == -1) {
+                break;
+            } else if (input <= 100 && input >= 0) {
+                pointCalc.add(input);
+            } 
+        }
+        System.out.println(pointCalc.pointAverage());
+        System.out.println(pointCalc.passingAverage());
+        System.out.println(pointCalc.passingPercentage());
+        this.pointCalc.gradeDistribution();
+    }
+    
+}
